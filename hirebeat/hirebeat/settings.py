ce8a0174c2adf910505c5c6d424eee31b8f24dbc
xuhang liu <xuhangliu@xuhangdeMBP.cable.rcn.com>
2020-07-25 00:49:59 -0400

    update settings

--- hirebeat/hirebeat/settings.py
+++ hirebeat/hirebeat/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'django',
 	'PASSWORD':'django',
     }
 }
