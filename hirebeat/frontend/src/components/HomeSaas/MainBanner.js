b029c8a450eccc680fa4d6220c910db197dfaa55
Xuhang Liu <xl@Xuhangs-MacBook-Pro.local>
2020-08-19 11:45:47 -0400

    refine template

--- hirebeat/frontend/src/components/HomeSaas/MainBanner.js
+++ hirebeat/frontend/src/components/HomeSaas/MainBanner.js
@@ -3,6 +3,22 @@ import {Link} from "react-router-dom";
 import ReactWOW from 'react-wow';
 import Particles from 'react-particles-js';
 import ModalVideo from 'react-modal-video';
+import arrow from "../public/images/saas-shape/arrow.png";
+import box1 from "../public/images/saas-shape/box1.png";
+import boy1 from "../public/images/saas-shape/boy1.png";
+import boy2 from "../public/images/saas-shape/boy2.png";
+import boy3 from "../public/images/saas-shape/boy3.png";
+import digitalscreen from "../public/images/saas-shape/digital-screen.png";
+import filter1 from "../public/images/saas-shape/filter1.png";
+import filter2 from "../public/images/saas-shape/filter2.png";
+import filter3 from "../public/images/saas-shape/filter3.png";
+import girl1 from "../public/images/saas-shape/girl1.png";
+import girl2 from "../public/images/saas-shape/girl2.png";
+import mainimage from "../public/images/saas-shape/main-image.png";
+import monitor from "../public/images/saas-shape/monitor.png";
+import saasmain from "../public/images/saas-shape/saas-main-image.png";
+import shape from "../public/images/saas-shape/shape-rotate.png";
+
 
 const particleOpt = {
     particles: {
@@ -43,12 +59,14 @@ class MainBanner extends Component {
         return (
             <React.Fragment>
                 {/* Popup Modal Video If you want to change the video need to update below videoID */}
+                <div>
                 <ModalVideo 
                     channel='youtube' 
                     isOpen={this.state.isOpen} 
                     videoId='_ysd-zHamjk' 
                     onClose={() => this.setState({isOpen: false})} 
                 />
+                </div>
 
                 <div className="saas-banner">
                     <div className="d-table">
@@ -58,56 +76,56 @@ class MainBanner extends Component {
                                     <div className="col-lg-6 col-md-12">
                                         <div className="saas-image mt-70">
                                             <ReactWOW animation='fadeInDown' delay='0.6s'>
-                                                <img src="/images/saas-shape/arrow.png" alt="arrow" />
+                                                <img src="../public/images/saas-shape/arrow.png" alt="arrow" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='fadeInUp' delay='0.6s'>
-                                                <img src="/images/saas-shape/box1.png" alt="box1" />
+                                                <img src={box1} alt="box1" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='fadeInLeft' delay='0.6s'>
-                                                <img src="/images/saas-shape/boy1.png" alt="boy1" />
+                                                <img src={boy1} alt="boy1" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='zoomIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/boy2.png" alt="boy2" />
+                                                <img src={boy2} alt="boy2" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='bounceIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/boy3.png" alt="boy3" />
+                                                <img src={boy3} alt="boy3" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='fadeInDown' delay='0.6s'>
-                                                <img src="/images/saas-shape/digital-screen.png" alt="Digital Screen" />
+                                                <img src={digitalscreen} alt="Digital Screen" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='zoomIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/filter1.png" alt="filter1" />
+                                                <img src={filter1} alt="filter1" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='fadeInUp' delay='0.6s'>
-                                                <img src="/images/saas-shape/filter2.png" alt="filter2" />
+                                                <img src={filter2} alt="filter2" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='rotateIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/filter3.png" alt="filter3" />
+                                                <img src={filter3} alt="filter3" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='fadeInUp' delay='0.6s'>
-                                                <img src="/images/saas-shape/girl1.png" alt="girl1" />
+                                                <img src={girl1} alt="girl1" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='zoomIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/girl2.png" alt="girl2" />
+                                                <img src={girl2} alt="girl2" />
                                             </ReactWOW>
 
                                             <ReactWOW animation='zoomIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/monitor.png" alt="monitor" />
+                                                <img src={monitor} alt="monitor" />
                                             </ReactWOW>
     
                                             {/* Main image */}
                                             <ReactWOW animation='zoomIn' delay='0.6s'>
-                                                <img src="/images/saas-shape/saas-main-image.png" alt="Main image" />
+                                                <img src={mainimage} alt="Main image" />
                                             </ReactWOW>
                                         </div>
                                     </div>
