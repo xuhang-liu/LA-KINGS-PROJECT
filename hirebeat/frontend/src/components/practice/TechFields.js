8e04a572cae484c38eaa220ee5cc236cf0d254dc
Xuhang Liu <xl@Xuhangs-MacBook-Pro.local>
2020-09-03 16:44:10 -0400

    update tech

--- hirebeat/frontend/src/components/practice/TechFields.js
+++ hirebeat/frontend/src/components/practice/TechFields.js
@@ -1,101 +1,174 @@
 import React, { Component, useState } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { SetupCard, CardRow, CardButton } from "./CardComponents";
 import { updateProfile } from "../../redux/actions/auth_actions";
 import { createMessage } from "../../redux/actions/message_actions";
+import PageTitleArea from '../Common/PageTitleArea';
+import emailjs from 'emailjs-com';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
-const techFields = [
-  "Design",
-  "Law",
-  "Retail",
-  "Accounting",
-  "Marketing",
-  "Consulting",
-  "Banking & Finance",
-  "Risk Management",
-  "Human Resources",
-  "Project Management",
-  "Computer Engineering",
-];
+import accounting from '../../assets/tech/Accounting.png';
+import admin from '../../assets/tech/Administrative.png';
+import consult from '../../assets/tech/Consulting.png';
+import finance from '../../assets/tech/Finance.png';
+import human from '../../assets/tech/Human.png';
+import market from '../../assets/tech/Marketing.png';
+import product from '../../assets/tech/Product.png';
+import retail from '../../assets/tech/Retail.png';
+import nofind from '../../assets/tech/Nofind.png';
 
-const FieldButtonGroup = (props) => {
-  const [field, setField] = useState("None");
 
-  const handleOnTap = () => {
-    const profile = {
-      user: props.user.id,
-      id: props.profile.id,
-      profession: field,
-    };
-    props.updateProfile(profile);
-    props.createMessage({ successMessage: "Feature coming soon" });
-  };
 
-  return (
-    <div style={{marginBottom:"10%"}}>
-      <div
-        className="btn-group btn-group-toggle"
-        data-toggle="buttons"
-        style={{ display: "flex", flexWrap: "wrap", justifyContent: "around" }}
-      >
-        {techFields.map((t, index) => {
-          return (
-            <label
-              className="btn btn-primary"
-              style={{
-                borderRadius: "5px",
-                marginRight: "15px",
-                marginBottom: "15px",
-                background: field == t ? "#4689FA" : "transparent",
-                boxShadow: "none",
-                border: "2px solid #91B8FB",
-                color: field == t ? "white" : "#91B8FB",
-              }}
-              key={index + "1"}
-            >
-              <input
-                type="radio"
-                name="options"
-                id={index}
-                key={index}
-                onClick={() => setField(t)}
-              />
-              {t}
-            </label>
-          );
-        })}
-      </div>
-      <br />
-      <br />
-      <CardRow>
-        <CardButton
-          onTap={handleOnTap}
-          textDisplayed={"Next"}
-          buttonWidth={"20%"}
-          btnClassName={"btn btn-warning"}
-        />
-      </CardRow>
-    </div>
-  );
-};
+function ScrollToTopOnMount() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return null;
+}
+
+function TechButtons() {
+  confirmAlert({
+    title: 'Feature is coming soon',
+    buttons: [
+      {
+        label: 'Ok'
+      }
+    ]
+    });
+}
 
 export class TechFields extends Component {
+
+  state = {
+    display: "hidden",
+  }
+
+  DisplayText = () => {
+    this.setState({
+      display: "",
+    });
+  }
+
   render() {
     return (
-      <SetupCard>
-        <CardRow>
-          <h5>Create A New Mock Interview</h5>
-        </CardRow>
-        <CardRow>
-          <h1>Choose Your Field</h1>
-        </CardRow>
-        <FieldButtonGroup
-          updateProfile={this.props.updateProfile}
-          createMessage={this.props.createMessage}
-          user={this.props.user}
-          profile={this.props.profile}
+      <React.Fragment>
+        <ScrollToTopOnMount />
+        <PageTitleArea 
+          pageTitle="Choose Your Field" 
+          pageDescription="Create A New Mock Interview" 
         />
-      </SetupCard>
+      <div style={{marginBottom:"10%", marginTop:"3%", marginLeft: "21%"}}>
+      <div>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={accounting}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={admin}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={consult}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={finance}/>
+      </button>  
+      </div>
+      <br/>
+      <div>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={human}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={market}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={product}/>
+      </button>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={TechButtons}>
+      <img src={retail}/>
+      </button>  
+      </div>
+      <div>
+      <button style={{
+        width: "15%",
+        border: "none",
+        marginLeft: "15px",
+        marginRight: "15px",
+        marginBottom: "15px",
+        backgroundColor: "white",}}
+        onClick={this.DisplayText}>
+      <img src={nofind}/>
+      </button>
+      <form style={{display:"inline", visibility:this.state.display, marginLeft: "15px"}}>
+      <input type="text" name="field" placeholder="What industry are you looking for?"
+      style={{border:"1px solid #E6E8F6", boxSizing: 'border-box', borderRadius:"3px", width:"20rem", fontSize:"0.75rem", height:"3rem"}}>
+      </input>
+      <button className="default-btn" style={{backgroundColor:"#56a3fa", marginLeft:"2%"}}>
+      <i className="bx bxs-hot"></i>
+        Submit
+      </button>
+      </form>
+      </div>
+    </div>
+    </React.Fragment>
     );
   }
 }
