36847ef4e02f8d8c836bfbfbe552e77407ce4673
Xuhang Liu <xl@Xuhangs-MacBook-Pro.local>
2020-11-10 16:07:53 -0500

    Production 101

--- hirebeat/frontend/src/components/blog/blog-details10.js
+++ hirebeat/frontend/src/components/blog/blog-details10.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PageTitleArea from '../Common/PageTitleArea';
-import BlogDetailsContent9 from './BlogDetailsContent9';
+import BlogDetailsContent10 from './BlogDetailsContent10';
 import FreeTrialArea from '../HomeSaas/FreeTrialArea';
 import { useEffect } from "react";
 import {Helmet} from "react-helmet";
@@ -19,16 +19,16 @@ class BlogDetails extends Component {
             <React.Fragment>
                 <Helmet>
                     <meta charSet="utf-8" />
-                    <title>HireBeat Blog – Tips for Getting Your Resume Past an Applicant Tracking System</title>
-                    <meta name="Description" CONTENT="Tips for Getting Your Resume Past an Applicant Tracking System"></meta>
-                    <link rel="canonical" href="https://hirebeat.co/tips-for-getting-your-resume-past-an-applicant-tracking-system"/>
+                    <title>HireBeat Blog – How To Answer The Question "Where Do You To See Yourself In 5 Years" In An Interview  (Sample Answer Included)</title>
+                    <meta name="Description" CONTENT="How To Answer The Question 'Where Do You To See Yourself In 5 Years' In An Interview  (Sample Answer Included)"></meta>
+                    <link rel="canonical" href="https://hirebeat.co/how-to-answer-the-question-where-do-you-to-see-yourself-in-5-years-in-an-interview"/>
                 </Helmet>
                 <ScrollToTopOnMount />
                 <PageTitleArea 
                     pageTitle="Blog Details" 
                     pageDescription="News and Insights" 
                 />
-                <BlogDetailsContent9 />
+                <BlogDetailsContent10 />
                 <FreeTrialArea />
             </React.Fragment>
         );
