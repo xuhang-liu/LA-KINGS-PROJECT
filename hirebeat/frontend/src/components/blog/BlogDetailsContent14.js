d0e3749e1e310bd24bb927c57b0bb929a08d89a5
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-01-13 12:29:15 -0500

    Production 148

--- hirebeat/frontend/src/components/blog/BlogDetailsContent14.js
+++ hirebeat/frontend/src/components/blog/BlogDetailsContent14.js
@@ -17,7 +17,7 @@ class BlogDetailsContent extends Component {
                                             <li>
                                                 <i className='bx bx-time'></i> 
                                                 <Link href="#">
-                                                    <a>December 08, 2020</a>
+                                                    <a>Janurary 13, 2021</a>
                                                 </Link>
                                             </li>
                                             <li>
@@ -27,23 +27,30 @@ class BlogDetailsContent extends Component {
                                         </ul>
                                     </div>
 
-                                    <h2>"Why do you want to work here?" Your Ultimate Interview Tips (Example included)</h2>
+                                    <h2>Interview Question "Why Do You Want to Work Here?" Ultimate Guide with Example</h2>
                                     <div className="article-image">
                                     <img src="https://hirebeat-assets.s3.amazonaws.com/blog/blog14-pic.jpg" alt="Interview answer, interview tips, job, career tips" />
                                     </div>
                                     <p>
-                                    Every job seeker wishes to get their dream job. They, therefore, research how to write the best resume and cover letter. They forget that the interview is an integral part of job hunting. The interview gives the employer a chance to assess whether you qualify for the job position.</p>
+                                    As a job seeker, you are very likely to encounter this question in a job interview. When an interviewer inquires, “why do you want to work here?” they are trying to evaluate whether you will be a valuable addition to their brand.</p>
                                     <p>
-                                    A great way to prepare when entering the interview room is by getting ready to respond to the interviewer effectively. Imagine how great it can be if you know exactly how to answer the commonly asked questions in the job interview. "Why do you want to work here?" seems to be a simple but challenging question. Lack of proper preparation for this question can be risky because you will lower the chances of acquiring the job. This article contains information on how to provide the answers to "why do you want to work here?"
+                                    An employee who is directly aligned with the values and mission of a company is deemed to be a productive asset and is guaranteed to work for the company longer. The hiring team is always interested in finding out whether you are that kind of employee.
+                                    </p>
+                                    <p>
+                                    You are likely to panic while figuring out an appropriate answer to that question. While a simple response such as, “I really need the money” is true, it doesn’t give you any credibility.
+                                    </p>
+                                    <p>
+                                    The best way to answer the question is by giving a response that shows you know some of the institution’s objectives and core values, and that you are an asset who is determined to achieve them when you get appointed for the job.
+                                    </p>
+                                    <p>
+                                    No hiring manager wants to hire a job seeker who is only after getting paid because they are attached to the company.
+                                    </p>
+                                    <p>
+                                    Here we are few tips for you to help you ace this question.
                                     </p>
                                     <br/>
-                                    <h3 className="mb-0">Why the interviewers ask, "Why do you want to work here?"</h3>
-                                    <br/>                                
-                                    <p>
-                                    Regardless of how the hiring manager phrase the question, "Why do you want to work here?" you must be ready to provide positive answers. He/she can phrase it in different ways, such as, "why choose our company?" "What made you apply for our job opportunity?" the interviewers ask this question to figure out whether you can fit in with the culture of their company. Also, they might want to know your motivation in applying for the job. An excellent way to answer this question is by referencing it to the organization's culture, mission, and vision. Also, it is essential to align it with your career goals and personal skills.
-                                    </p>
                                     <h3 className="mb-0">How to answer the question</h3>
-                                    <br/>
+                                    <br/>                                
                                     <p>
                                     The company aims to hire someone whose goal is to make a positive impact on the organization. Thus, you need to come up with good ideas. Think of why the organization wants to hire you. Figure out what you want to offer to the company more than the benefit you will get from the job. Here are steps to prepare your answer to this question:
                                     </p>
@@ -51,25 +58,42 @@ class BlogDetailsContent extends Component {
                                     ✅  Research the company online
                                     </h4>
                                     <p>
-                                    As technology advances, many organizations change their ways of running their businesses. Thus, it is essential to research the business website before attending the interview. Know the products and services they are offering and understand the current changes in their company. Also, think of the knowledge and experience you have and how it can benefit the company.
+                                    Take some time to comprehend everything you can learn about the enterprise. If they have a website, visit the “About Us” section, or even search for them on social media. Look out for articles and news about projects that they might be undertaking.
+                                    </p>
+                                    <p>
+                                    If you don’t discover anything that intrigues you and plays with your imagination in a way that drives your desire through the roof, you probably won’t be able to impress the interview team with your response.
                                     </p>
                                     <h4>
                                     ✅  Understand your past career success
                                     </h4>
                                     <p>
-                                    It is good to relate this question to your previous jobs. Figure out the things you did to meet the employers' expectations and the ideas you introduced to help the company succeed. Also, understand the lessons you learned from your previous jobs and how you can apply them in the future.
+                                    If you find interesting information about the company that you are interested to work for, your response needs to be tuned in a way that coincides with the company’s values and tie to your career experience.
+                                    </p>
+                                    <p>
+                                    For instance, instead of giving an answer such as, "I love this place and I would like to be part of its workforce", think of a response that demonstrates how your personal goals are directly related to the organization’s objectives or values.
+                                    </p>
+                                    <p>
+                                    For example, if your interview is with a company that treasures community service, mention how your love for voluntary work will propel you to be a steadfast part of its community service team.
                                     </p>
                                     <br/>
-                                    <h3 className="mb-0">Example of the answer to "Why do you want to work here?"</h3>
+                                    <h3 className="mb-0">Sample Answer</h3>
                                     <br/>
                                     <p>
-                                    "I have been working in the hospitality industry for more than 6 years. I have extended my experience in the food and beverage production and I am ready to apply the expertise in your organization. I love working in a team and I am excited that this role will allow me to use my skills to produce excellent foods and drinks. I have been a loyal guest in your hotel, and I'm impressed by the cutting edge technology and excellent services. I researched your company's mission, value, and culture, all of which made me believe that this is the best place I would love to work. I would love to grow my skills and career in a company that is passionate about innovation and success".
+                                    "I am a firm believer of the collaborative approach when dealing with projects. I knew that I had to apply to this position to become part of your production team. After watching one of your production videos that showcased your team’s effort, I really got inspired.
                                     </p>
                                     <p>
-                                    How to respond to "Why do you want to work here?" depends on the organization. It is good to know how to formulate a meaningful response that matches different interview situations. Remember to avoid answers such as "I truly need a job and this opportunity seems to be interesting" or "I heard that you have a good pay."
+                                    My background in production has given me some preparation for this role and my love for teamwork in the pursuit of a common goal coincides with the values put forward by the video. I look forward to working as one of the contributors of value in your company."
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    The above response presents every aspect out there. The hiring managers learns that you already know the company well because you have seen its team in action.
                                     </p>
                                     <p>
-                                    Looking to improve your interview skills? Try a mock interview at <Link to="/">HireBeat</Link> and get professional feedback on your performance.
+                                    By relating your personal values and job experience in the response, you leave no doubt that you are the suitable candidate for the job.
+                                    </p>
+                                    <br/>
+                                    <p>
+                                    Looking to further improve your interview skills? Try a mock interview at <a href="/">HireBeat</a> and get professional feedback on your performance.
                                     </p>
                                 </div>
                             </div>
@@ -78,7 +102,7 @@ class BlogDetailsContent extends Component {
                                     <li>
                                     <FacebookShareButton 
                                         url={"https://hirebeat.co/blog-why-do-you-want-to-work-here"}
-                                        quote={"'Why do you want to work here?' Your Ultimate Interview Tips"}
+                                        quote={"Interview Question 'Why do you want to work here?'"}
                                         hashtag="#hirebeat">
                                         <a target="_blank">
                                             <i className="bx bxl-facebook"></i>
@@ -88,7 +112,7 @@ class BlogDetailsContent extends Component {
                                     <li>
                                         <TwitterShareButton
                                            url={"https://hirebeat.co/blog-why-do-you-want-to-work-here"}
-                                           title={"'Why do you want to work here?' Your Ultimate Interview Tips"}
+                                           title={"Interview Question 'Why do you want to work here?'"}
                                            via={"HireBeat"}
                                            hashtag="#hirebeat">
                                            <a target="_blank">
@@ -99,7 +123,7 @@ class BlogDetailsContent extends Component {
                                     <li>
                                         <LinkedinShareButton
                                             url={"https://hirebeat.co/blog-why-do-you-want-to-work-here"}
-                                            title={"'Why do you want to work here?' Your Ultimate Interview Tips"}
+                                            title={"Interview Question 'Why do you want to work here?'"}
                                             source={"HireBeat"}>
                                             <a target="_blank">
                                                 <i className="bx bxl-linkedin"></i>
@@ -109,7 +133,7 @@ class BlogDetailsContent extends Component {
                                     <li>
                                         <WhatsappShareButton
                                             url={"https://hirebeat.co/blog-why-do-you-want-to-work-here"}
-                                            title={"'Why do you want to work here?' Your Ultimate Interview Tips"}>
+                                            title={"Interview Question 'Why do you want to work here?'"}>
                                             <a target="_blank">
                                                 <i className="bx bxl-whatsapp"></i>
                                             </a>
