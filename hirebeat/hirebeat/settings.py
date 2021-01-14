9a96b95083c3fa44a65c3aa68d6f6c676d4303dd
SHAO BIN DANIEL SHI HONG <shong@wisc.edu>
2021-01-14 13:55:45 -0500

    dd

--- hirebeat/hirebeat/settings.py
+++ hirebeat/hirebeat/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'shaobindanielhong',
         'PASSWORD': os.getenv('DATABASE_SECRET_KEY'),
     }
 }
