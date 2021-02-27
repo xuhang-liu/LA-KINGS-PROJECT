66bbd7d21c2e83a4cd0eb71efa9c5ab1faf8f887
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-02-27 17:40:05 -0500

    Production 222

--- hirebeat/frontend/src/components/accounts/EmailVerification.js
+++ hirebeat/frontend/src/components/accounts/EmailVerification.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+//import {Link} from "react-router-dom";
 import PageTitleArea from '../Common/PageTitleArea';
 import { useEffect } from "react";
 import {connect} from "react-redux";
