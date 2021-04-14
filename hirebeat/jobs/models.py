a3e39bda2f1618a1ad96982c730777054986693f
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-04-14 17:43:14 -0400

    refine apply_candidates

--- hirebeat/jobs/models.py
+++ hirebeat/jobs/models.py
@@ -24,9 +24,11 @@ class ApplyCandidates(models.Model):
     candidate = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
-    phone_num = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
     resume_url = models.CharField(max_length=100, null=True, blank=True)
+    apply_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.job_title
