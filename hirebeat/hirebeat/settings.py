42252a95531e2f79588f347c0bfca46cd632de50
Liam Zeng the_bear <leamzeng@gmail.com>
2021-04-20 15:07:56 -0400

    a

--- hirebeat/hirebeat/settings.py
+++ hirebeat/hirebeat/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'django',
+        'USER': 'postgres',
         'PASSWORD': os.getenv('DATABASE_SECRET_KEY'),
     }
 }
