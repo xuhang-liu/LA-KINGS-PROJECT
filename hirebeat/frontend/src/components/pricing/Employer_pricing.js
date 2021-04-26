b1143211e93c917c0381d3f434623d49463f7af7
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-04-26 16:05:40 -0400

    add roi

--- hirebeat/frontend/src/components/pricing/Employer_pricing.js
+++ hirebeat/frontend/src/components/pricing/Employer_pricing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PageTitleArea1 from '../Common/PageTitleArea1';
 import Employer_PricingStyleOne from './Employer_PricingStyleOne';
+import ROICalculator from '../Common/ROICalculator';
 //import FaqContent from '../Faq/FaqContent';
 //import FreeTrialArea from '../HomeSaas/FreeTrialArea';
 import { useEffect } from "react";
@@ -39,6 +40,7 @@ class Employer_pricing extends Component {
                     pageDescription="Hiring talents, and take back your time. We are here to help you save 50% of your workday on screening calls." 
                 />
                 <Employer_PricingStyleOne />
+                <ROICalculator/>
                 <Footer />
             </React.Fragment>
             </DocumentMeta>
