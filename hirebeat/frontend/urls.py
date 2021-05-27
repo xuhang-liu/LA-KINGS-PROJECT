780cfb8bc347cd2152009d13451de314da4ee9fb
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-05-26 20:49:24 -0400

    Production 305

--- hirebeat/frontend/urls.py
+++ hirebeat/frontend/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)  # when in root path, run that index method to render the html
+    path('',views.index),  # when in root path, run that index method to render the html
+    path('job-seekers', views.job_seekers)
 ]
 
 # This url needs to be included in the hirebeat/urls.py to work
