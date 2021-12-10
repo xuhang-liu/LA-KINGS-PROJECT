9b589303aadeaa0bff1aa7b2514ca898ebfed04e
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2021-12-10 12:24:37 -0500

    Production 405

--- hirebeat/hirebeat/settings.py
+++ hirebeat/hirebeat/settings.py
@@ -95,20 +95,20 @@ WSGI_APPLICATION = 'hirebeat.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'django',
-    #     'PASSWORD': os.getenv('DATABASE_SECRET_KEY'),
-    # }
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'postgres',
-    'USER': 'django',
-    'PASSWORD': os.getenv('RDS_DATABASE_SECRET_KEY'),
-    'HOST': 'database-hirebeat-app.c9lge1nbo9rx.us-east-1.rds.amazonaws.com',
-    'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'django',
+        'PASSWORD': os.getenv('DATABASE_SECRET_KEY'),
     }
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.postgresql',
+    # 'NAME': 'postgres',
+    # 'USER': 'django',
+    # 'PASSWORD': os.getenv('RDS_DATABASE_SECRET_KEY'),
+    # 'HOST': 'database-hirebeat-app.c9lge1nbo9rx.us-east-1.rds.amazonaws.com',
+    # 'PORT': '5432',
+    # }
 }
 
 
