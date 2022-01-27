eed678ad2336f36a0dfa261e17f238e50ec5cca7
xuhang-liu <74516382+xuhang-liu@users.noreply.github.com>
2022-01-27 15:20:14 -0500

    Production 414

--- hirebeat/frontend/src/components/accounts/EmployerRegister.js
+++ hirebeat/frontend/src/components/accounts/EmployerRegister.js
@@ -59,10 +59,12 @@ export class EmployerRegister extends Component {
   getParams =() => {
     let params = [];
     let email = window.location.search;
-    email = email.substring(1, email?.length); // remove "?" from uri
-    email = window.atob(email); // decode
-    if (email.startsWith("email")){
-      let param = email.split("=")[1]; // get value
+    email = email?.substring(1, email?.length); // remove "?" from uri
+    if (email?.startsWith("ZW1haWw")){
+      email = window?.atob(email); // decode
+    }
+    if (email?.startsWith("email")){
+      let param = email?.split("=")[1]; // get value
       params.push(param);
     }
     return params;
